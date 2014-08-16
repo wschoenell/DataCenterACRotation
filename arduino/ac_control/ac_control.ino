2fe3e36911e48e33918946f1fbb5ec1d109cd488
William Schoenell <wschoenell@gmail.com>
2014-08-16 03:10:31 +0200

    Cleaned up a little bit... WIP

--- arduino/ac_control/ac_control.ino
+++ arduino/ac_control/ac_control.ino
@@ -1,20 +1,34 @@
-//the following line tells the compiler that
-//timer0_millis is declared somewhere else
-// extern unsigned long timer0_millis;
-
-#include <EEPROM.h>
-
 /*
- AC datacenter rotator
+               AC datacenter rotator
 */
 
 #define PROG_VERSION 0.1
 
 /*
+		LOAD LIBRARIES
+*/
+
+//the following line tells the compiler that
+//timer0_millis is declared somewhere else
+// extern unsigned long timer0_millis;
+
+// - Ethernet library
+#include <SPI.h>
+#include <Ethernet.h>
+
+// - Temp and humidity sensor
+#include <dht11.h> 
+dht11 DHT11;
+
+#include <EEPROM.h>
+
+
+/*
 		CONFIGURATIONS
 */
-#define DHT11PIN 2
 #define BUTTON1PIN 0
+
+#define DHT11PIN 2
 
 #define AC1PIN 9
 #define AC2PIN 8
@@ -26,28 +40,11 @@ long temperatureInterval = EEPROM.read(2) * 1000; // in seconds
 long rotationInterval = EEPROM.read(3) * 3600000 + EEPROM.read(4) * 60000; // hours + minutes
 
 /*
-		LOAD LIBRARIES
+		MISC VARIABLES
 */
 
-// - Ethernet library
-#include <SPI.h>
-#include <Ethernet.h>
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-//IPAddress ip(192,168,1,50);
-
-// - SD Card library
-// #include <SD.h>
-
-// - Temp and humidity sensor
-#include <dht11.h> 
-dht11 DHT11;
-
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// 80 is the default HTTP port. 
-EthernetServer server(80);
-
-///////////////////////
+// Variables to the EthernetServer
+  EthernetServer server(80); 
 String read_string = String(50); //string for fetching data from address
 String cmd = String(10);
 String arg = String(10);
@@ -57,7 +54,7 @@ byte ind1 = 0;
 byte ind2 = 0;
 ///////////////////////
 
-// ------------------ FUNCTIONS -------------------------
+byte cmd_status; // Status of the requested command. 0 = No requested command. 1 = Success. 2 = Fail. 3 = Unknown command.
 long time_now;
 long temperatureTime; // next millis() which should check the temperature
 long rotationTime; // next millis() which should rotate the ACs
@@ -65,6 +62,9 @@ boolean ac_all = false; // If true, will START with both engines.
 int ac_now = 1; // Will start with engine 0
 byte chk; // To DHT11
 
+/*
+		FUNCTIONS
+*/
 
 // Maximum temperature check function
 // Checks periodically the local temperature. If goes over a treshold, turn the two AC units.
@@ -75,10 +75,10 @@ void temperature_check()
   chk = DHT11.read(DHT11PIN);
 
   if (chk == DHTLIB_OK) {
-      Serial.print("   Humidity: ");
-      Serial.println(DHT11.humidity);
       Serial.print("   Temperature: ");
       Serial.println(DHT11.temperature);
+      Serial.print("   Humidity: ");
+      Serial.println(DHT11.humidity);
       Serial.print("   Check interval: ");
       Serial.print(temperatureInterval);
       Serial.println(" ms.");
@@ -100,6 +100,8 @@ void temperature_check()
     }
 }
 
+
+// Turns ON/OFF the ACs
 void check_ac()
 {
 	if (ac_all == true)    // Turn both ACs
@@ -119,6 +121,8 @@ void check_ac()
 	}
 }
 
+
+// Rotate the ACs
 void ac_rotate()
 {
       	Serial.println("-- AC rotating function called --");
@@ -140,18 +144,23 @@ void ac_rotate()
         
 }		
 
+// Run commands via HTTP requests
 void run_command()
 {
-	Serial.print("Running command ");
+/*	Serial.print("   Running command ");
 	Serial.print(cmd);
 	Serial.print(" with ");
 	Serial.print(arg);
-	Serial.println(" as argument.");
+	Serial.println(" as argument."); */
 	
 	/// COMMANDS section
-}  
-
-// ------------------ END FUNCTIONS ------------------
+        if (cmd = "caca"){
+          Serial.println("   Running cacaoo");
+          cmd_status = 1;
+        } else {
+          cmd_status = 3;
+        }
+} 
 
 /*
 		SETUP AND INIT
@@ -171,19 +180,21 @@ void setup()
   pinMode(AC2PIN, OUTPUT);
   digitalWrite(AC1PIN, HIGH);
   digitalWrite(AC2PIN, HIGH);
-  temperatureTime = millis() + 24000; //0;   // Set the 1st temperature check to 4 minutes from now
-  rotationTime = millis() + 30000; //0;      // Set the 1st rotation check to 5 minutes from now
+  temperatureTime = millis() + 240000;   // Set the 1st temperature check to 4 minutes from now
+  rotationTime = millis() + 300000;      // Set the 1st rotation check to 5 minutes from now
   
   // CONFIGURE SERIAL PORT FOR DEBUGGING //
   Serial.begin(57600);
   Serial.println("AC ROTATOR - William Schoenell - <william@iaa.es>");
+  Serial.println("http://github.com/wschoenell/DataCenterACRotation");
   Serial.print("DHT11 LIBRARY VERSION: ");
   Serial.println(DHT11LIB_VERSION);
   Serial.print("PROGRAM VERSION: ");
   Serial.println(PROG_VERSION);
   Serial.println();
 
-  // Start the Ethernet connection and the server:
+  // Start the Ethernet connection and the server //
+  byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
   Ethernet.begin(mac); // Automatic IP via DHCP , ip);
   server.begin();
   Serial.print("server is at ");
@@ -198,7 +209,7 @@ void loop()
 {
   EthernetClient client = server.available();
   if (client) {
-    Serial.println("new client");
+    Serial.println("-- New Ethernet Client --");
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
 
@@ -215,23 +226,27 @@ void loop()
 			  ind1 = read_string.indexOf('=');
 			  ind2 = read_string.indexOf('&');
 			  cmd = read_string.substring(ind1+1, ind2);
-			  Serial.print("Command is: ");
+			  Serial.print("   Command is: ");
 			  Serial.println(cmd);
 			  ind1 = read_string.indexOf("arg");
 			  if(ind1 >=0){
 			    ind1 = ind1+4;
 				ind2 = read_string.indexOf(" ", ind1);
 				arg = read_string.substring(ind1, ind2);
-				Serial.print("Argument is: ");
+				Serial.print("   Argument is: ");
 				Serial.println(arg);
-				Serial.println(read_string);
+//				Serial.println(read_string);
 				if (arg.length() != 0)
 					run_command();
-			  }
+			  }else{
+                cmd_status = 0;
+            }
            }
           read_string = "";
 		  cmd = "";
 		  arg = "";
+
+          chk = DHT11.read(DHT11PIN);
         
           // send a standard http response header
           client.println("HTTP/1.1 200 OK");
@@ -239,21 +254,25 @@ void loop()
           client.println("Connection: close");  // the connection will be closed after completion of the response
 //	      client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          chk = DHT11.read(DHT11PIN);
-          client.print("Temperature: ");
+//          client.println("<!DOCTYPE HTML>");
+//          client.println("<html>");
+          client.println("# temp,hum,cfg_temp_low,cfg_temp_upp,cfg_rot_interval,cmd_status");
           client.print((float)DHT11.temperature, 2);
-          client.println("<br />");
-          client.print("Humidity: ");
+          client.print(",");
           client.print((float)DHT11.humidity, 2);
-          client.println("<br />");
-          client.print("Configuration temp_upp, temperatureInterval, rotationInterval: ");
+          client.print(",");
+          client.print((float)temp_low, 1);
+          client.print(",");
           client.print((float)temp_upp, 1);
+          client.print(",");
           client.print((float)temperatureInterval, 1);
+          client.print(",");
           client.print((float)rotationInterval, 1);
-          client.println("<br />");
-          client.println("</html>");
+          client.print(",");
+          client.print(cmd_status);
+          client.println();
+//          client.println("<br />");
+//          client.println("</html>");
           break;
         }
         if (c == '\n') {
@@ -270,7 +289,7 @@ void loop()
     delay(1);
     // close the connection:
     client.stop();
-    Serial.println("client disonnected");
+    Serial.println("   Client disonnected.");
   }
   
   time_now = millis(); // millisec
